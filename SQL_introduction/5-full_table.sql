e21b53ac1c4418d1b2e79a2c25b510113309a3d0
eftelakacori <eftelakacori92@gmail.com>
2025-01-14 08:23:57 +0000

    List tables

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
+--Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 SELECT table_name, column_name, column_type
 FROM information_schema.columns
 WHERE table_schema = DATABASE() AND table_name = 'first_table';
