e81e43395c23b1d2422e2aa2f0c29a6c4fb3461a
eftelakacori <eftelakacori92@gmail.com>
2025-01-14 10:21:25 +0000

    First table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,6 @@
---Write a script that creates a table called first_table
+-- Write a script that creates a table called first_table
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
