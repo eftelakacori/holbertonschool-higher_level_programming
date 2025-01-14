c2d8886f78b4eecaaf916da09506bf2b982a7748
eftelakacori <eftelakacori92@gmail.com>
2025-01-14 10:25:29 +0000

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
---Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT table_name, column_name, column_type, is_nullable, column_default, extra
-FROM information_schema.columns
-WHERE table_schema = DATABASE() AND table_name = 'first_table';
+-- This script will print the description of the first_table in the current database
+SHOW CREATE TABLE first_table;
